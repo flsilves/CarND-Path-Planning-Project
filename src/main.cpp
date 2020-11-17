e7804287799820a6c3dc2a5608d66c6620d745bb
Flavio Silvestre <flaviosilvestre89@gmail.com>
2020-11-17 23:21:48 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,9 @@ static std::ofstream telemetry_log("./telemetry.log", std::ios::app);
 
 class VehicleState {
  public:
-  VehicleState(json telemetry_data) {
+  VehicleState() = default;
+
+  void update(json telemetry_data) {
     x = telemetry_data["x"];
     y = telemetry_data["y"];
     s = telemetry_data["s"];
@@ -64,8 +66,18 @@ class VehicleState {
 
 class Path {
  public:
-  Path(json telemetry_data, const std::string& path_name = "Path") {
-    name = path_name;
+  // Path() = default;
+
+  Path(string path_name = "Path") { name = path_name; }
+
+  Path(const Path& other, const std::string& path_name = "Path")
+      : name(path_name),
+        x(other.x),
+        y(other.y),
+        end_s(other.end_s),
+        end_d(other.end_d) {}
+
+  void update(json telemetry_data) {
     x = telemetry_data["previous_path_x"].get<vector<double>>();
     y = telemetry_data["previous_path_y"].get<vector<double>>();
     end_s = telemetry_data["end_path_s"];
@@ -75,13 +87,6 @@ class Path {
       throw std::runtime_error("<Path::ctor>: x and y have different lengths");
     }
   }
-
-  Path(const Path& other, const std::string& path_name = "Path")
-      : name(path_name),
-        x(other.x),
-        y(other.y),
-        end_s(other.end_s),
-        end_d(other.end_d) {}
 
   bool empty() const { return x.empty(); }
   size_t size() const { return x.size(); }
@@ -157,8 +162,146 @@ class SensorData {
 
 class TrajectoryGenerator {
  public:
+  TrajectoryGenerator(const Path& previous_path, const VehicleState& ego_state,
+                      const MapWaypoints& map)
+      : previous_path_(std::ref(previous_path)),
+        ego_state_(std::ref(ego_state)),
+        map(map) {}
+
+  void update_input(const Path& previous_path, const VehicleState& ego_state) {
+    previous_path_ = std::ref(previous_path);
+    ego_state_ = std::ref(ego_state);
+  }
+
+  Path generate_trajectory(double target_velocity, int target_lane) {
+    vector<double> anchor_x;
+    vector<double> anchor_y;
+    int lane = 0;
+    double ref_x = ego_state_.get().x;
+    double ref_y = ego_state_.get().y;
+    double ref_yaw = deg2rad(ego_state_.get().yaw);
+
+    size_t prev_size = previous_path_.get().size();
+
+    // Initialization when there's no previous points
+    if (prev_size < 2) {
+      // Extrapolate the previous position based on current yaw angle
+      double prev_car_x = ego_state_.get().x - cos(ref_yaw);
+      double prev_car_y = ego_state_.get().y - sin(ref_yaw);
+
+      // std::cout << "Ego.x:" << prev_car_x << " -> " << ego_state_.x
+      //          << std::endl;
+      // std::cout << "Ego.y:" << prev_car_y << " -> " << ego_state_.y
+      //          << std::endl;
+
+      anchor_x.push_back(prev_car_x);
+      anchor_x.push_back(ego_state_.get().x);
+
+      anchor_y.push_back(prev_car_y);
+      anchor_y.push_back(ego_state_.get().y);
+    } else {
+      ref_x = previous_path_.get().x.end()[-1];
+      ref_y = previous_path_.get().y.end()[-1];
+
+      double ref_x_prev = previous_path_.get().x.end()[-2];
+      double ref_y_prev = previous_path_.get().y.end()[-2];
+
+      ref_yaw = atan2(ref_y - ref_y_prev,
+                      ref_x - ref_x_prev);  // where is this used?? Why do we
+                                            // need it? can't we use just yaw?
+
+      // std::cout << "ref_yaw:" << rad2deg(ref_yaw) << '\n';
+
+      anchor_x.push_back(ref_x_prev);
+      anchor_x.push_back(ref_x);
+
+      anchor_y.push_back(ref_y_prev);
+      anchor_y.push_back(ref_y);
+    }
+
+    // In Freenet add evenly 30 spaced points ahead of the starting
+    // reference
+    vector<double> next_wp0 = getXY(ego_state_.get().s + 50,
+                                    (2 + 4 * target_lane), map.s, map.x, map.y);
+
+    vector<double> next_wp1 = getXY(ego_state_.get().s + 100,
+                                    (2 + 4 * target_lane), map.s, map.x, map.y);
+
+    vector<double> next_wp2 = getXY(ego_state_.get().s + 150,
+                                    (2 + 4 * target_lane), map.s, map.x, map.y);
+
+    anchor_x.push_back(next_wp0[0]);
+    anchor_x.push_back(next_wp1[0]);
+    anchor_x.push_back(next_wp2[0]);
+
+    anchor_y.push_back(next_wp0[1]);
+    anchor_y.push_back(next_wp1[1]);
+    anchor_y.push_back(next_wp2[1]);
+
+    for (int i = 0; i < anchor_x.size(); ++i) {
+      // shift car reference angle to 0 degrees
+      double shift_x = anchor_x[i] - ref_x;
+      double shift_y = anchor_y[i] - ref_y;
+
+      anchor_x[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+      anchor_y[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+    }
+
+    tk::spline spline;
+    spline.set_points(anchor_x, anchor_y);
+
+    // start with all of the previous path points from last frame
+    generated_path = previous_path_.get();
+
+    // Calculate how to break up spline points so that we travel at our
+    // desired reference velocity
+    double target_x = 30.0;
+    double target_y = spline(target_x);
+    double target_dist =
+        sqrt((target_x) * (target_x) + (target_y) * (target_y));
+
+    double x_add_on = 0;
+    double N = target_dist / (.02 * target_velocity / 2.2369);
+
+    // std::cout << "N:" << N << std::endl;
+
+    // include acceleration for trajectory generation
+    for (int i = 1; i <= 50 - prev_size; ++i) {
+      // distance =  N (point) * 0.02
+      // (second/point) * v (miles/second)
+      double x_point = x_add_on + (target_x) / N;
+
+      double y_point = spline(x_point);
+
+      // std::cout << "Points: x[" << x_point << "] y[" << y_point << "]
+      // "
+      //          << std::endl;
+
+      x_add_on = x_point;
+
+      double x_ref = x_point;
+      double y_ref = y_point;
+
+      x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw) + ref_x;
+      y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw) + ref_y;
+
+      // std::cout << "Appending: x[" << x_point << "] y[" << y_point <<
+      // "] "
+      //          << std::endl;
+
+      generated_path.x.push_back(x_point);
+      generated_path.y.push_back(y_point);
+    }
+    return generated_path;
+  }
+
+  Path get_path() { return generated_path; }
+
  public:
-  std::vector<double> anchors;
+  Path generated_path{"generated"};
+  std::reference_wrapper<const Path> previous_path_;
+  std::reference_wrapper<const VehicleState> ego_state_;
+  MapWaypoints map;
 };
 
 std::ostream& operator<<(std::ostream& os, const SensorData& data) {
@@ -175,9 +318,14 @@ int main() {
   double target_velocity = 0;  // mph
   int lane = 1;
 
-  h.onMessage([&map, &target_velocity, &lane](uWS::WebSocket<uWS::SERVER> ws,
-                                              char* data, size_t length,
-                                              uWS::OpCode opCode) {
+  VehicleState ego_state;
+  Path previous_path("previous_path");
+  TrajectoryGenerator trajectory_generator(previous_path, ego_state, map);
+
+  h.onMessage([&map, &target_velocity, &lane, &previous_path, &ego_state,
+               &trajectory_generator](uWS::WebSocket<uWS::SERVER> ws,
+                                      char* data, size_t length,
+                                      uWS::OpCode opCode) {
     if (valid_socket_message(length, data)) {
       auto s = hasData(data);
 
@@ -188,18 +336,15 @@ int main() {
         if (event == "telemetry") {
           auto telemetry_data = j[1];
 
-          // std::cout << "json" << j << std::endl;
+          ego_state.update(telemetry_data);
+          previous_path.update(telemetry_data);
 
-          VehicleState ego(telemetry_data);
-          Path previous_path(telemetry_data, "previous_path");
           SensorData sensor_fusion{telemetry_data["sensor_fusion"]};
-
-          TrajectoryGenerator trajectory_generator{};
 
           int prev_size = previous_path.size();
 
           bool too_close = sensor_fusion.vehicle_close_ahead(
-              previous_path.size(), previous_path.end_s, ego.get_lane());
+              previous_path.size(), previous_path.end_s, ego_state.get_lane());
 
           std::cout << sensor_fusion << '\n';
 
@@ -211,165 +356,14 @@ int main() {
             target_velocity += .224;
           }
 
-          vector<double> anchor_x;
-          vector<double> anchor_y;
-
-          double ref_x = ego.x;
-          double ref_y = ego.y;
-          double ref_yaw = deg2rad(ego.yaw);
-
-          // Initialization when there's no previous points
-          if (prev_size < 2) {
-            // Extrapolate the previous position based on current yaw angle
-            double prev_car_x = ego.x - cos(ref_yaw);
-            double prev_car_y = ego.y - sin(ref_yaw);
-
-            std::cout << "Ego.x:" << prev_car_x << " -> " << ego.x << std::endl;
-            std::cout << "Ego.y:" << prev_car_y << " -> " << ego.y << std::endl;
-
-            anchor_x.push_back(prev_car_x);
-            anchor_x.push_back(ego.x);
-
-            anchor_y.push_back(prev_car_y);
-            anchor_y.push_back(ego.y);
-          } else {
-            ref_x = previous_path.x.end()[-1];
-            ref_y = previous_path.y.end()[-1];
-
-            double ref_x_prev = previous_path.x.end()[-2];
-            double ref_y_prev = previous_path.y.end()[-2];
-
-            ref_yaw =
-                atan2(ref_y - ref_y_prev,
-                      ref_x - ref_x_prev);  // where is this used?? Why do we
-                                            // need it? can't we use just yaw?
-
-            std::cout << "ref_yaw:" << rad2deg(ref_yaw) << '\n';
-
-            anchor_x.push_back(ref_x_prev);
-            anchor_x.push_back(ref_x);
-
-            anchor_y.push_back(ref_y_prev);
-            anchor_y.push_back(ref_y);
-          }
-
-          // In Freenet add evenly 30 spaced points ahead of the starting
-          // reference
-          vector<double> next_wp0 =
-              getXY(ego.s + 50, (2 + 4 * lane), map.s, map.x, map.y);
-
-          vector<double> next_wp1 =
-              getXY(ego.s + 100, (2 + 4 * lane), map.s, map.x, map.y);
-
-          vector<double> next_wp2 =
-              getXY(ego.s + 150, (2 + 4 * lane), map.s, map.x, map.y);
-
-          // std::cout << "WP0[ x:" << next_wp0[0] << " y:" << next_wp0[1] <<
-          // "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "WP1[ x:" << next_wp1[0] << " y:" << next_wp1[1] <<
-          // "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "WP2[ x:" << next_wp2[0] << " y:" << next_wp2[1] <<
-          // "]
-          // "
-          //          << std::endl;
-
-          anchor_x.push_back(next_wp0[0]);
-          anchor_x.push_back(next_wp1[0]);
-          anchor_x.push_back(next_wp2[0]);
-
-          anchor_y.push_back(next_wp0[1]);
-          anchor_y.push_back(next_wp1[1]);
-          anchor_y.push_back(next_wp2[1]);
-
-          for (int i = 0; i < anchor_x.size(); ++i) {
-            // shift car reference angle to 0 degrees
-            double shift_x = anchor_x[i] - ref_x;
-            double shift_y = anchor_y[i] - ref_y;
-
-            anchor_x[i] =
-                (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
-            anchor_y[i] =
-                (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
-          }
-
-          // std::cout << "AN0[ x:" << anchor_x[0] << " y:" << anchor_y[0] << "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "AN1[ x:" << anchor_x[1] << " y:" << anchor_y[1] << "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "AN2[ x:" << anchor_x[2] << " y:" << anchor_y[2] << "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "AN3[ x:" << anchor_x[3] << " y:" << anchor_y[3] << "]
-          // "
-          //          << std::endl;
-          //
-          // std::cout << "AN4[ x:" << anchor_x[4] << " y:" << anchor_y[4] << "]
-          // "
-          //          << std::endl;
-
-          //  set (x,y) points to the spline
-          tk::spline spline;
-          spline.set_points(anchor_x, anchor_y);
-
-          // start with all of the previous path points from last frame
-          Path next_path{previous_path, "next"};
-
-          // Calculate how to break up spline points so that we travel at our
-          // desired reference velocity
-          double target_x = 30.0;
-          double target_y = spline(target_x);
-          double target_dist =
-              sqrt((target_x) * (target_x) + (target_y) * (target_y));
-
-          double x_add_on = 0;
-          double N = target_dist / (.02 * target_velocity / 2.2369);
-
-          // std::cout << "N:" << N << std::endl;
-
-          // include acceleration for trajectory generation
-          for (int i = 1; i <= 50 - prev_size; ++i) {
-            // distance =  N (point) * 0.02
-            // (second/point) * v (miles/second)
-            double x_point = x_add_on + (target_x) / N;
-
-            double y_point = spline(x_point);
-
-            // std::cout << "Points: x[" << x_point << "] y[" << y_point << "] "
-            //          << std::endl;
-
-            x_add_on = x_point;
-
-            double x_ref = x_point;
-            double y_ref = y_point;
-
-            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw) + ref_x;
-            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw) + ref_y;
-
-            // std::cout << "Appending: x[" << x_point << "] y[" << y_point <<
-            // "] "
-            //          << std::endl;
-
-            next_path.x.push_back(x_point);
-            next_path.y.push_back(y_point);
-          }
-
-          std::cout << ego << '\n';
+          std::cout << ego_state << '\n';
 
           if (not previous_path.empty()) {
             std::cout << previous_path << '\n';
           }
 
+          auto next_path =
+              trajectory_generator.generate_trajectory(target_velocity, lane);
           std::cout << next_path << '\n' << std::endl;
 
           json msgJson;
